218b901f807889d9be715cd0c56eb41285392f0d
abhi114 <nabhi531@gmail.com>
2024-09-15 20:56:46 +0530

    canvas done

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,24 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
     const canvasRef = useRef<HTMLCanvasElement>(null); //used to store a value in a component but dont want to re render
     const [isDrawing,setIsDrawing] = useState(false);
+    useEffect(()=>{
+        //initialize the canvas elements
+        const canvas = canvasRef.current;
+        if(canvas){
+            const ctx = canvas.getContext('2d');
+            if(ctx){
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight - canvas.offsetTop;
+                ctx.lineCap = 'round' //for brush type
+                ctx.lineWidth = 3;
+
+            }
+        }
+    },[])
     //mouse click event for start drawing
+    //<HTMLCanvasElement>: This is a generic parameter that specifies which HTML element the event is associated with
     const startDrawing = (e:React.MouseEvent<HTMLCanvasElement>)=>{
         const canvas = canvasRef.current;
         // setting background color and 2d context to draw 2d drawings
@@ -13,7 +28,7 @@ export default function Home() {
                 const ctx = canvas.getContext('2d');
                 if(ctx){
                     ctx.beginPath();
-                    ctx.moveTo(e.nativeEvent.offsetX,e.nativeEvent.offsetY);
+                    ctx.moveTo(e.nativeEvent.offsetX,e.nativeEvent.offsetY); //// The X and y coordinate of the mouse event relative to the canvas
                     setIsDrawing(true);
                 }
         }
@@ -21,11 +36,25 @@ export default function Home() {
     const stopDrawing = ()=>{
         setIsDrawing(false);
     }
-    
+    const draw = (e:React.MouseEvent<HTMLCanvasElement>)=>{
+        if(!isDrawing){
+            return;
+        }
+        const canvas = canvasRef.current;
+        if(canvas){
+            const ctx = canvas.getContext('2d');
+            if(ctx){
+                ctx.strokeStyle = 'white';
+                ctx.lineTo(e.nativeEvent.offsetX,e.nativeEvent.offsetY);
+                ctx.stroke();
+            }
+        }
+    };
+
     return (
         <>
-        <canvas ref={canvasRef} id="canvas" className="absolute top-0 left-0 w-full h-full" onMouseDown={startDrawing} onMouseOut={stopDrawing}
-        onMouseUp={stopDrawing}/>
+        <canvas ref={canvasRef} id="canvas" className="absolute top-0 left-0 w-full h-full bg-black" onMouseDown={startDrawing} onMouseOut={stopDrawing}
+        onMouseUp={stopDrawing} onMouseMove={draw}/>
         </>
     )
 
