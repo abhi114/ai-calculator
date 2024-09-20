1e577464cd04a55d17837cd42b3dba70596c51d5
abhi114 <nabhi531@gmail.com>
2024-09-20 18:19:56 +0530

    fixed

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -210,7 +210,7 @@ export default function Home() {
             <Draggable
             key={index}
             defaultPosition={latexPosition}
-            onStop={(e,data)=> setLatexPosition({x:data.x,y:data.y})}
+            onStop={(_e,data)=> setLatexPosition({x:data.x,y:data.y})}
             >
                 <div className="absolute text-white">
                        <div className="latex-content">{latex}</div>
